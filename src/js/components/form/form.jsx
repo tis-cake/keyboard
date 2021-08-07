42e11fd8e836d30c27b39ce37a726e7aff92d5b4
Tis <tisbootis@gmail.com>
2021-08-07 19:22:06 +0300

    add basic functionality for Keyboard element

--- src/js/components/form/form.jsx
+++ src/js/components/form/form.jsx
@@ -3,24 +3,49 @@ import InputMask from 'react-input-mask';
 
 import { AppRoute } from '../../const';
 import { PageContext } from '../../context';
+import { undoNum, addNum } from '../../utils/number';
 
-const REGEXP_PATTERN = /\d/g;
-const MAX_COUNT_NUMBERS = 11;
+const MAX_COUNT_DIGIT = 11;
+const TEL_INIT_VALUE = '+7(___)___-__-__';
+const TEL_UNDO_VALUE = 'UNDO';
 
 function Form() {
   const setCurrentPage = useContext(PageContext);
-  const [tel, setTel] = useState('');
+  const [tel, setTel] = useState(TEL_INIT_VALUE);
   const [checked, setChecked] = useState(false);
 
-  // const handleTelChange = (evt) => {
-  //   setTel(evt.currentTarget.value);
-  // };
+  const handleTelChange = (evt) => {
+    if (!evt.currentTarget.value) {
+      return;
+    }
+    setTel(evt.currentTarget.value);
+  };
 
-  // const handleCheckboxChange = () => {
-  //   setChecked(!checked);
-  // };
+  const handleKeyboardClick = (evt) => {
+    if (evt.target === evt.currentTarget) {
+      return;
+    }
 
-  const isValidityTel = (!!tel && tel.match(REGEXP_PATTERN).length === MAX_COUNT_NUMBERS);
+    setTel((prev) => {
+      if (evt.target.value === TEL_UNDO_VALUE) {
+        return undoNum(prev);
+      }
+
+      return addNum(prev, evt.target.value);
+    });
+  };
+
+  const handleKeyboardFocus = (evt) => {
+    if (evt.target === evt.currentTarget) {
+      return;
+    }
+
+    console.log(evt.target);
+  };
+
+  console.log(tel);
+
+  const isValidityTel = (!!tel && tel.match(/\d/g).length === MAX_COUNT_DIGIT);
   const isValidityCheckbox = checked;
   const isValidityForm = (isValidityTel && isValidityCheckbox);
 
@@ -36,30 +61,35 @@ function Form() {
 
       <InputMask
         mask="+7(999)999-99-99"
-        placeholder="+7(___)___-__-__"
+        placeholder={TEL_INIT_VALUE}
         className="form__input"
         type="tel"
         name="TEL"
         required
-        onChange={(evt) => setTel(evt.currentTarget.value)}
+        value={tel}
+        onChange={(evt) => handleTelChange(evt)}
       />
 
       <p className="form__desc">
         и с Вами свяжется наш менеждер для дальнейшей консультации
       </p>
 
-      <div className="form__keyboard keyboard">
-        <button className="keyboard__btn btn" type="button">1</button>
-        <button className="keyboard__btn btn" type="button">2</button>
-        <button className="keyboard__btn btn" type="button">3</button>
-        <button className="keyboard__btn btn" type="button">4</button>
-        <button className="keyboard__btn btn" type="button">5</button>
-        <button className="keyboard__btn btn" type="button">6</button>
-        <button className="keyboard__btn btn" type="button">7</button>
-        <button className="keyboard__btn btn" type="button">8</button>
-        <button className="keyboard__btn btn" type="button">9</button>
-        <button className="keyboard__btn keyboard__btn--reset btn" type="button">Стереть</button>
-        <button className="keyboard__btn btn" type="button">0</button>
+      <div
+        className="form__keyboard keyboard"
+        onClick={handleKeyboardClick}
+        onFocus={handleKeyboardFocus}
+      >
+        <button className="keyboard__btn btn" type="button" value="1">1</button>
+        <button className="keyboard__btn btn" type="button" value="2">2</button>
+        <button className="keyboard__btn btn" type="button" value="3">3</button>
+        <button className="keyboard__btn btn" type="button" value="4">4</button>
+        <button className="keyboard__btn btn" type="button" value="5">5</button>
+        <button className="keyboard__btn btn" type="button" value="6">6</button>
+        <button className="keyboard__btn btn" type="button" value="7">7</button>
+        <button className="keyboard__btn btn" type="button" value="8">8</button>
+        <button className="keyboard__btn btn" type="button" value="9">9</button>
+        <button className="keyboard__btn keyboard__btn--reset btn" type="button" value={TEL_UNDO_VALUE}>Стереть</button>
+        <button className="keyboard__btn btn" type="button" value="0">0</button>
       </div>
 
       <input
---
+++ src/js/utils/number.js
@@ -0,0 +1,27 @@
+const MIN_COUNT_DIGIT = 1;
+const CHAR_MASK = '_';
+
+const undoNum = (currentNum) => {
+  const numbers = currentNum.match(/\d/g);
+
+  if (numbers.length === MIN_COUNT_DIGIT) {
+    return currentNum;
+  }
+
+  const lastNumber = numbers[numbers.length - 1];
+  const lastNumberIndex = currentNum.lastIndexOf(lastNumber);
+
+  const chars = currentNum.split('');
+  chars[lastNumberIndex] = CHAR_MASK;
+
+  return chars.join('');
+};
+
+const addNum = (currentNum, currentValue) => {
+  return currentNum.replace(CHAR_MASK, currentValue);
+};
+
+export {
+  undoNum,
+  addNum,
+};
